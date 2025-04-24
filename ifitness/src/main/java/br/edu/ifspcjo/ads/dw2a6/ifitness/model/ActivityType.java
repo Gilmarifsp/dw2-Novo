e05b61b8668f9b1125281c69536210af6ecd51d6
CJ3011348 <CJ3011348@192.168.56.1>
2025-04-24 20:45:25 -0300

    Aula 24-04-25

---
+++ ifitness/src/main/java/br/edu/ifspcjo/ads/dw2a6/ifitness/model/ActivityType.java
@@ -0,0 +1,19 @@
+package br.edu.ifspcjo.ads.dw2a6.ifitness.model;
+
+public enum ActivityType {
+
+	CAMINHADA ("Caminhada"),
+    CICLISMO ("Ciclismo"),
+    CORRIDA ("Corrida"),
+    NATACAO ("Natação");
+
+    private String type;
+
+    ActivityType(String type) {
+        this.type = type;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+}
---
+++ ifitness/src/main/java/br/edu/ifspcjo/ads/dw2a6/ifitness/repository/ActivityRepository.java
@@ -0,0 +1,10 @@
+package br.edu.ifspcjo.ads.dw2a6.ifitness.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.edu.ifspcjo.ads.dw2a6.ifitness.model.Activity;
+
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
+
+}
+
