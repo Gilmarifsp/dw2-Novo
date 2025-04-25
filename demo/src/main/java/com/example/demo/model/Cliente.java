1231edf85295db429e7743fa997a7ac338834dd5
CJ3011348 <CJ3011348@192.168.56.1>
2025-04-24 22:35:47 -0300

    exe atualizado

--- demo/src/main/java/com/example/demo/model/Cliente.java
+++ demo/src/main/java/com/example/demo/model/Cliente.java
@@ -1,5 +1,6 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,6 +19,9 @@ public class Cliente {
 	private String telefone;
 	private String cpf;
 	private Boolean ativo;
+	@Embedded
+	private EnderecoCliente endereco;
+	
 
 	public Long getId() {
 		return id;
@@ -67,4 +71,15 @@ public class Cliente {
 		this.ativo = ativo;
 	}
 
+	public EnderecoCliente getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(EnderecoCliente endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+
 }
+
